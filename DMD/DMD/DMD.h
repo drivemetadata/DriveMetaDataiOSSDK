b681fa3951eb60364df3e107499ac52c4f997395
DriveMetaData <drivemetadata@192.168.1.29>
2024-02-25 16:50:53 +0530

    Add Framework

---
+++ DMD/DMD/DMD.h
@@ -0,0 +1,18 @@
+//
+//  DMD.h
+//  DMD
+//
+//  Created by DriveMetaData on 18/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DMD.
+FOUNDATION_EXPORT double DMDVersionNumber;
+
+//! Project version string for DMD.
+FOUNDATION_EXPORT const unsigned char DMDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMD/PublicHeader.h>
+
+
---
+++ DMD/DMD/DriveMetaData.swift
@@ -0,0 +1,18 @@
+//
+//  DriveMetaData.swift
+//  DMD
+//
+//  Created by DriveMetaData on 18/02/24.
+//
+
+import Foundation
+
+public class DriveMetaData
+{
+    private init(){}
+    
+    public static func doSomething()->String
+    {
+        return "First Progect"
+    }
+}
