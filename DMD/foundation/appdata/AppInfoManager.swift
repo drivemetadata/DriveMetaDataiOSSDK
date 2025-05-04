29ea9886d45301435e190b1e3a743841a6529482
Amit Kumar Gupta <Amit606@users.noreply.github.com>
2025-05-04 17:12:07 +0530

    Update the podspec values

--- DMD/foundation/appdata/AppInfoManager.swift
+++ DMD/foundation/appdata/AppInfoManager.swift
@@ -30,7 +30,7 @@ class AppInfoManager {
     /// Retrieves library details
     func getLibraryDetails() -> [String: Any] {
         return [
-            "sdk_version": "1.0.2"
+            "sdk_version": "1.0.4"
         ]
     }
 }
